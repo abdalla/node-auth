e9bc93545e9759f7227eb54aa92786ad5294ea5a
Carlos Abdalla <abdalla@msdev.com.br>
2017-12-27 18:50:41 -0500

    code climate 2

--- src/controllers/middleware/required-token.js
+++ src/controllers/middleware/required-token.js
@@ -2,9 +2,7 @@
 const requiredToken =  ( options, tokenValidator ) => {
 	return function requiredToken(req, res, next) {
 
-		if (!options.publicKey) { 
-			throw 'publicKey is required';
-		}
+		if (!options.publicKey) { throw 'publicKey is required'; }
 
 		const ignoreRoute = ( options.ignoredRoutes || false ) && options.ignoredRoutes.some( route => route === req.originalUrl );
 
