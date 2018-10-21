4f42930c6c0fe645c1a3e1d1be13ad949e083c8c
Carlos Abdalla <abdalla@msdev.com.br>
2018-10-21 19:53:03 -0300

    comment

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+//TODO: database should be into env file
 module.exports = {
 	database: 'mongodb://localhost:27017/node-auth', // If you are not running through docker you have to change here.
 	token: {
