f5f1e166dbdf6b400b69b857c2b0272a7a191340
Tadeu Camargo <tadeu.camargo@accurate.com.br>
2019-12-02 22:32:50 -0300

    Fixed old var

--- src/controllers/middleware/required-token.js
+++ src/controllers/middleware/required-token.js
@@ -6,7 +6,7 @@ const requiredToken =  ( options, tokenValidator ) => (req, res, next) => {
 	const ignoreRoute = ( options.ignoredRoutes || false ) && options.ignoredRoutes.some( route => route === req.originalUrl );
 
 	if (!ignoreRoute) {
-		var token = req.body.token || req.query.token || req.headers['x-access-token'];
+		const token = req.body.token || req.query.token || req.headers['x-access-token'];
 
 		if (!token) {
 			return res.status(403).send({
