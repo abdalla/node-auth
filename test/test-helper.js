f2565ebf6a177d33d7542c3a7607cd0772ca4074
Carlos Abdalla <abdalla@msdev.com.br>
2021-09-28 13:59:53 -0300

    .

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,13 +1,18 @@
 'use strict';
-import MongodbMemoryServer from 'mongodb-memory-server';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import db from '../src/db';
 
-const server = new MongodbMemoryServer();
+let server;
+
 /*
  * Creates and/or connects to a mongo test database in memory
  */
 const createDB = async () => {
-	const uri = await server.getUri();
+	if (!server) {
+		server = await MongoMemoryServer.create();
+	}
+
+	const uri = server.getUri();
 	
 	await db.connect(uri);
 };
